d32e8060cfa6e5705a5d318517d337c5f782e815
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-01-30 17:41:24 -0500

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -13,11 +13,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
 
-	newnode = malloc(sizeof(binary_tree_t));
-
-	if (!newnode)
-		return (NULL);
 	if (!parent)
+		return (NULL);
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if (!newnode)
 		return (NULL);
 
 	newnode->n = value;
