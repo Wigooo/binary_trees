f3f5a430600248ecf254b386915aa6bef59046db
Mohamed <abdoashraf973@gmail.com>
2024-01-30 23:27:30 +0200

    Added Print file

--- binary_trees.h
+++ binary_trees.h
@@ -14,13 +14,13 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-};
+}binary_tree_t;
 
 /**
  * struct levelorder_queue_s - Level order traversal queue.
@@ -37,6 +37,7 @@ typedef struct levelorder_queue_s
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_root(const binary_tree_t *node);
+void binary_tree_print(const binary_tree_t *);
 
 
 #endif /* BINARY_TREES_H */
