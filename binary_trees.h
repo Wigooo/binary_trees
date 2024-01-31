12557ba9ecda5e68003a38d18e6a8b2a065dde9a
Mohamed <abdoashraf973@gmail.com>
2024-01-31 16:22:25 +0200

    Added Task 33 and updated the header file

--- binary_trees.h
+++ binary_trees.h
@@ -69,6 +69,7 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *bst_search(const bst_t *tree, int value);
 avl_t *avl_insert(avl_t **tree, int value);
+avl_t *avl_remove(avl_t *root, int value);
 
 
 #endif
