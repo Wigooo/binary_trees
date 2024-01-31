72cde75989c84cd5ee60d780e84231f4d3537150
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-01-30 19:25:30 -0500

    11-binary_tree_size.c

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size -  measures the size of a binary tree
+ * @tree: root of tree
+ * Return: size if success, 0 otherwise
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t nodesize = 0;
+	/*size_t nodesize = sizeleft + sizeright;*/
+
+	if (!tree)
+		return (0);
+
+	nodesize = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+
+	return (nodesize);
+}
