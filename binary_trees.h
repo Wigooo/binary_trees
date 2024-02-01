15f80f4e9fb9133f992bee0e3f5d89be183a8b22
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-02-01 12:55:24 -0500

    header file

--- binary_trees.h
+++ binary_trees.h
@@ -80,5 +80,8 @@ bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
 avl_t *array_to_avl(int *array, size_t size);
+avl_t *sorted_array_to_avl(int *array, size_t size);
+int binary_tree_is_heap(const binary_tree_t *tree);
+
 
 #endif
